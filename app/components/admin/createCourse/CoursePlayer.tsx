f43a4fa5c7f4e90c0758725ddbd87786b25c1e6b
badhon <mylaptop512@gmail.com>
2024-01-18 19:43:10 +0600

    new file added

---
+++ app/components/admin/createCourse/CoursePlayer.tsx
@@ -0,0 +1,14 @@
+import React,{FC} from 'react'
+
+type Props = {
+    videoUrl:any
+    title:any
+}
+
+const CoursePlayer:FC<Props> = ({}) => {
+  return (
+    <div>CoursePlayer</div>
+  )
+}
+
+export default CoursePlayer
--- app/components/admin/createCourse/CourseReview.tsx
+++ app/components/admin/createCourse/CourseReview.tsx
@@ -1,30 +1,30 @@
-import React ,{FC}from 'react'
+import React, { FC } from "react";
+import CoursePlayer from "./CoursePlayer";
 
 type Props = {
-    Active:number;
-    setActive:(active:number)=>void;
-    courseData:any
-    handelCourseCreate:any
-}
+  Active: number;
+  setActive: (active: number) => void;
+  courseData: any;
+  handelCourseCreate: any;
+};
 
-const CourseReview:FC<Props> = ({
-    Active,
-    setActive,
-    courseData,
-    handelCourseCreate  
+const CourseReview: FC<Props> = ({
+  Active,
+  setActive,
+  courseData,
+  handelCourseCreate,
 }) => {
   return (
-    <div className='w-[90%] m-auto py-5 mb-5'>
-        <div className='w-full relative'>
-            <div className='w-full mt-10'>
-                <CoursePlayer
-                
-                videoUrl={courseData?.demoUrl}
-                title={courseData?.title}/>
-            </div>
+    <div className="w-[90%] m-auto py-5 mb-5">
+      <div className="w-full relative">
+        <div className="w-full mt-10">
+          <CoursePlayer
+            videoUrl={courseData?.demoUrl}
+            title={courseData?.title}
+          />
         </div>
+      </div>
     </div>
-  )
-}
-4
-export default CourseReview
+  );
+};
+export default CourseReview;
