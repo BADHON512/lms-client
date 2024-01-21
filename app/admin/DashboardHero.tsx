5305b70d8e38f7a5fde68ef44e153738c6bc94ff
badhon <mylaptop512@gmail.com>
2024-01-21 23:36:25 +0600

    fetch the live course now

--- app/admin/DashboardHero.tsx
+++ app/admin/DashboardHero.tsx
@@ -1,24 +1,22 @@
-"use client"
-import React, { FC } from 'react'
-import DashboardHeader from './DashboardHeader'
-import CreateCourse from '../components/admin/createCourse/CreateCourse'
+"use client";
+import React, { FC } from "react";
+import DashboardHeader from "./DashboardHeader";
+import CreateCourse from "../components/admin/createCourse/CreateCourse";
 
 type Props = {
-  select:number
-  setSelect:(select:number)=>void
-}
+  select: number;
+  setSelect: (select: number) => void;
+};
 
-const DashboardHero:FC<Props> = ({select,setSelect}) => {
+const DashboardHero: FC<Props> = ({ select, setSelect }) => {
   return (
-    <div className='min-h-screen'>
-        <DashboardHeader/>
-        {
-        select===4&&(
-          <CreateCourse/>
-        )
-        }
-    </div>
-  )
-}
+    <div className="min-h-screen">
+      <DashboardHeader />
+      {select === 4 && <CreateCourse />}
 
-export default DashboardHero
+      {select === 5 && <h1>all course live</h1>}
+    </div>
+  );
+};
+
+export default DashboardHero;
