e16137841f5c5a19094ed609d55d2e8db8daf045
badhon <mylaptop512@gmail.com>
2024-01-22 16:56:42 +0600

    light and dark

--- app/components/admin/createCourse/allCourse/AllCourse.tsx
+++ app/components/admin/createCourse/allCourse/AllCourse.tsx
@@ -1,56 +1,123 @@
-import { Box, Button } from '@mui/material'
-import { DataGrid } from '@mui/x-data-grid'
-import { useTheme } from 'next-themes'
-import React from 'react'
-import { AiOutlineDelete } from 'react-icons/ai'
+import { Box, Button } from "@mui/material";
+import { DataGrid } from "@mui/x-data-grid";
+import { useTheme } from "next-themes";
+import React from "react";
+import { AiOutlineDelete } from "react-icons/ai";
 
-
-type Props = {}
+type Props = {};
 
 const AllCourse = (props: Props) => {
-    const [theme,setTheme]=useTheme()
+  const { theme, setTheme } = useTheme();
 
-    const colums=[
-        {
-            field:'ID',headerName:'ID',flex:0.5
-        },
-        {
-            field:'Title',headerName:'Course Title',flex:1
-        },
-        {
-            field:'Ratings',headerName:'Ratings',flex:0.5
-        },
-        {
-            field:'Purchesed',headerName:'Purchesed',flex:0.5
-        },
-        {
-            field:'Createed_at',headerName:'Createed_at',flex:0.5
-        },
-        {
-            field:"",
-            headerName:'Delete',
-            flex:0.2,
-            renderCell:()=>{
-                return(
-                    <Button>
-                    
-                    <AiOutlineDelete
-                    className='dark:text-white text-black'/>
-                    </Button>
-                )
-            }
-        }
-
-    ]
-    const rows = [
-        { ID: 1, Title: 'Snow', Ratings: '5start', Purchesed: 14,Createed_at:"2/5/2024" },
- 
-      ];
+  const colums = [
+    {
+      field: "id",
+      headerName: "ID",
+      flex: 0.5,
+    },
+    {
+      field: "Title",
+      headerName: "Course Title",
+      flex: 1,
+    },
+    {
+      field: "Ratings",
+      headerName: "Ratings",
+      flex: 0.5,
+    },
+    {
+      field: "Purchesed",
+      headerName: "Purchesed",
+      flex: 0.5,
+    },
+    {
+      field: "Createed_at",
+      headerName: "Createed_at",
+      flex: 0.5,
+    },
+    {
+      field: "",
+      headerName: "Delete",
+      flex: 0.2,
+      renderCell: () => {
+        return (
+          <Button>
+            <AiOutlineDelete className="dark:text-white text-black" />
+          </Button>
+        );
+      },
+    },
+  ];
+  const rows = [
+    {
+      id: 145721,
+      Title: "Snow",
+      Ratings: "5start",
+      Purchesed: 14,
+      Createed_at: "2/5/2024",
+    },
+  ];
   return (
-    <div className='mt-[120px]'>
+    <div className="mt-[120px] ">
+      <Box>
+        <Box
+          height="80vh"
+          sx={{
+            "& .MuiDataGrid-root": {
+              border: "none",
+              outline: "none",
+            },
+            "& .css-pqjvzy-MuiSvgIcon-root-MuiSelect-icon": {
+              color: theme === "dark" ? "#fff" : "#000",
+            },
+            "& .MuiDataGrid-sortIcon": {
+              color: theme === "dark" ? "#fff" : "#000",
+            },
+            "& .MuiDataGrid-row": {
+              color: theme === "dark" ? "#fff" : "#000",
+              borderBottom:
+                theme === "dark"
+                  ? "1px solid #ffffff30 !important"
+                  : "1px solid #ccc !important",
+            },
+            "& .MuiTablePagination-root": {
+              color: theme === "dark" ? "#fff" : "#000",
+            },
 
+            "& .MuiDataGrid-cell": {
+              borderBottom: "none",
+            },
+            "&.name-column--cell": {
+              color: theme === "dark" ? "#fff" : "#000",
+            },
+            "& .MuiDataGrid-columnHeaders": {
+              backgroundColor: theme === "dark" ? "#3e4396" : "#A4A9FC",
+              borderBottom: "none",
+              color: theme === "dark" ? "#fff" : "#000",
+            },
+            "& .MuiDataGrid-virtualScroller": {
+              backgroundColor: theme === "dark" ? "#1F2A40" : "#F2F0F0",
+            },
+
+            "& .MuiDataGrid-footerContainer": {
+              color: theme === "dark" ? "#fff" : "#000",
+              borderTop: "none",
+              backgroundColor: theme === "dark" ? "#3e4396" : "#A4A9FC",
+            },
+            "& .MuiCheckbox-root": {
+              color:
+                theme === "dark" ? `#b7ebde !important` : `#000 !important`,
+            },
+            "& .MuiCheckbox-toolbarContainer .MuiButton-text": {
+              color: `#000 !important`,
+            },
+          }}
+        >
+          <DataGrid checkboxSelection rows={rows} columns={colums} />
+        </Box>
+      </Box>
     </div>
-  )
-}
+  );
+};
 
-export default AllCourse
+export default AllCourse;
