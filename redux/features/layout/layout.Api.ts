2a082e71b16c9bbd30e3f8cdba2a8a5c0525f971
badhon <mylaptop512@gmail.com>
2024-02-01 20:40:07 +0600

    one file name is layout

--- redux/features/layout/layout.Api.ts
+++ redux/features/layout/layout.Api.ts
@@ -3,8 +3,8 @@ import { apiSlice } from "../api/apiSlice";
 const layOutApi=apiSlice.injectEndpoints({
     endpoints:(builder)=>({
         getBanner:builder.query({
-            query:()=>({
-                url:"/banner",
+            query:(type)=>({
+                url:`get-layout/${type}`,
                 method:"GET",
                 credentials:"include" as const
         })
