34d6215c4d0e082556b4b5a783dc978bdc722312
badhon <mylaptop512@gmail.com>
2024-02-01 23:47:42 +0600

    working

--- app/components/admin/Hero/HeroCustomize.tsx
+++ app/components/admin/Hero/HeroCustomize.tsx
@@ -1,52 +1,97 @@
-import React,{useState,FC, useEffect} from 'react'
-import { AiOutlineCamera } from 'react-icons/ai'
+import { style } from "@/app/styles/styels";
+import { useGetBannerQuery } from "@/redux/features/layout/layout.Api";
+import React, { useState, FC, useEffect } from "react";
+import { AiOutlineCamera } from "react-icons/ai";
 
-type Props = {}
+type Props = {};
 
 const HeroCustomize = (props: Props) => {
-    const [image, setImage] = useState('')
-    const [title, setTitle] = useState('')
-    const [subTitle, setSubTitle] = useState('')
-    
-//  useEffect(()=>{
-//     if(data){
-//         setTitle(data?.layout?.banner?.title)
-//         setSubTitle(data?.layout?.banner?.subTitle)
-//         setImage(data?.layout?.banner?.image?.url)
-//     }
-//  },[data])
- const handleUpdate = (e:any) => {
-    const file = e.target.files[0]
-    const reader = new FileReader()
-    reader.readAsDataURL(file)
-    reader.onloadend = () => {
-        setImage(reader.result as string)
+  const [image, setImage] = useState("");
+  const [title, setTitle] = useState("");
+  const [subTitle, setSubTitle] = useState("");
+  const { data, isLoading } = useGetBannerQuery("Banner", {
+    refetchOnMountOrArgChange: true,
+  });
+  console.log(title);
+
+  useEffect(() => {
+    if (data) {
+      setTitle(data?.layout[0]?.banner?.title);
+      setSubTitle(data?.layout[0]?.banner?.subTitle);
+      setImage(data?.layout[0]?.banner?.image?.url);
     }
- }
+  }, [data]);
+
+ 
+  const handleUpdate = (e: any) => {
+    const file = e.target.files?.[0];
+    if(file){
+         const reader = new FileReader();  
+         reader.onload=(e:any)=>{
+            if(reader.readyState===2){
+                setImage(e.target.result as string);
+            }
+            }
+         }
+         
+    }
+
   return (
-    <div className='mt-[200px]'>
-        <div className='w-full 1000px:flex items-center'>
-            <div className="absolute  1000px:top-[unset] 1500px:w-[700px]  1100px:h-[500px] 1100px:w-[500px] w-[50vh] h-[50vh] hero_animation rounded-[50%] 1100px:left-[18rem] 1500px:left-[21rem]">
-                <div className="1000px:w-[40%] flex 1000px:min-h-screen items-center justify-end pt-[70px] 1000px:pt-[0px] z-[10]">
-                <div className="relative flex items-center justify-end">
-                
-                <img src={image} alt="image not found"
-                className='object-contain 1100px:max-w-[90%] w-[90%] 1500px:max-w-[85%] h-[auto] z-[10]' />
-                <input type="file" 
-                id='banner'
-                accept='image/*'
-                onChange={handleUpdate}
-                className='hidden'/>
-                <label htmlFor="banner" className='cursor-pointer absolute bottom-0 right-0 z-20'>
-                <AiOutlineCamera className='dark:text-white text-base text-[18px] cursor-pointer'/>
-                </label>
+    <div className="mt-[200px]">
+      <div className="w-full 1000px:flex items-center">
+        <div className="flex justify-center ">
+          <div className="relative w-[50vh] h-[50vh] rounded-full border border-[green]">
+            <div className=" w-[30vh] h-[30vh] 300px:w-[40vh] 300px:h-[40vh] 400px:w-[30vh]  400px:h-[30vh] 800px:h-[50vh] 800px:w-[50vh] rounded-full hero_animation absolute "></div>
 
-                </div>
-                </div>
-            </div>
+            <img
+              alt="img not found"
+              src={image}
+              className="h-[50vh] w-[50vh] rounded-full   object-contain absolute  "
+            />
+            <input
+              type="file"
+              id="banner"
+              accept="image/*"
+              onChange={handleUpdate}
+              className="hidden"
+            />
+            <label
+              htmlFor="banner"
+              className="cursor-pointer absolute bottom-9 right-12 "
+            >
+              <AiOutlineCamera
+                size={25}
+                className="dark:text-white text-base  cursor-pointer"
+              />
+            </label>
+          </div>
         </div>
-    </div>
-  )
-}
+        <div className="1000px:w-[55%]  ml-10 flex flex-col items-center ">
+          <textarea
+         
+            className=" dark:text-white text-[#000000c7]  bg-transparent   outline-none focus:border-none text-[40px] font-Josefin font-[500] leading-[45px] resize-none "
+            cols={30}
+            rows={2}
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+          <br />
+          <textarea
+            cols={50}
+            rows={3}
+            value={subTitle}
+            onChange={(e) => setSubTitle(e.target.value)}
+            className="dark:text-white text-[#000000c7]  resize-none!border-none bg-transparent font-Josefin font-[600] outline-none "
+          />
+          <br />
 
-export default HeroCustomize
+          <div className="h-full w-full ">
+            <button className={`${style.button} !w-[150px]`}>Save</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HeroCustomize;
