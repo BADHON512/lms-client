3bd6ce03c0e2db5434c907f7b9193b0436adffb0
badhon <mylaptop512@gmail.com>
2024-02-20 21:25:10 +0600

    payment intigration done

--- app/course/payment/ChackOutForm.tsx
+++ app/course/payment/ChackOutForm.tsx
@@ -1,7 +1,10 @@
+import { style } from '@/app/styles/styels'
 import { useLoadUserQuery } from '@/redux/features/api/apiSlice'
 import { useCreateOrderMutation } from '@/redux/features/orders/ordersApi'
 import { LinkAuthenticationElement, PaymentElement, useElements, useStripe } from '@stripe/react-stripe-js'
-import React, { useState } from 'react'
+import { redirect } from 'next/navigation'
+import React, { useEffect, useState } from 'react'
+import toast from 'react-hot-toast'
 
 
 type Props = {
@@ -19,7 +22,37 @@ const CheckOutForm = ({setOpen,data}: Props) => {
      const {}=useLoadUserQuery({skip:loadUser?false:true})
      const [isLoading, setIsLoading] = useState(false)
      const handleSubmit = async (e:any) => {
+      e.preventDefault()
+      if (!stripe || !elements) {
+        return
+      }
+      setIsLoading(true)
+       const {error,paymentIntent}=await stripe.confirmPayment({
+        elements,
+        redirect: 'if_required',
+
+       })
+       if(error ){
+        setMessage(error.message )
+        setIsLoading(false)
+       }else if( paymentIntent&& paymentIntent.status==='succeeded'){
+        setIsLoading(false)
+        createOrder({courseId:data?._id,payment_info:paymentIntent})
+       }
      }
+
+     useEffect(()=>{
+      if(orderData){
+        setLoadUser(true)
+        redirect(`/course-access/${data._id}`)
+      }
+     if(error ){
+      if('data' in error){
+        const errorMessage=error as any
+        toast.error(errorMessage.data.message)
+      }
+     }
+     },[orderData,error])
   return (
     <form id="payment-form" onSubmit={handleSubmit}>
     <LinkAuthenticationElement id="link-authentication-element"
@@ -33,12 +66,12 @@ const CheckOutForm = ({setOpen,data}: Props) => {
       />
     <PaymentElement id="payment-element" />
     <button disabled={isLoading || !stripe || !elements} id="submit">
-      <span id="button-text">
-        {isLoading ? <div className="spinner" id="spinner"></div> : "Pay now"}
+      <span id="button-text" className={`${style.button} mt-2 !h-[35px] min-w-[100px]`}>
+        {isLoading ? 'Paying...' : "Pay now"}
       </span>
     </button>
     {/* Show any error or success messages */}
-    {message && <div id="payment-message">{message}</div>}
+    {message && <div id="payment-message" className='text-[red] font-Poppins pt-2'>{message}</div>}
   </form>
   )
 }
