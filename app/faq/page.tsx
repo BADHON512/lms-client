de7ef28dfa344e4dea07d9eb72c101368129aafd
badhon <mylaptop512@gmail.com>
2024-03-03 20:21:22 +0600

    new two pages added

---
+++ app/faq/page.tsx
@@ -0,0 +1,45 @@
+'use client'
+import React, { useState } from 'react'
+import Heading from '../utils/Heading'
+import Header from '../components/Header'
+import Footer from '../components/footer/Footer'
+import Faq from '../components/Faq/Faq'
+
+type Props = {}
+
+const Page = (props: Props) => {
+
+    const [route, setRoute] = useState('Login')
+    const [open, setOpen] = useState(false)
+    return (
+        <div className='min-h-screen'>
+            <Heading
+                title='About'
+                description='Elearning is programming community'
+                keyword='Programming community coding skills expert insights collaborating growth' />
+            <Header
+                route={route}
+                setRoute={setRoute}
+                open={open}
+                setOpen={setOpen}
+                activeItem={4} />
+
+            <div className="w-full mb-8">
+
+
+
+
+                <Faq />
+
+
+
+
+
+
+            </div>
+            <Footer />
+        </div>
+    )
+}
+
+export default Page
---
+++ app/policy/page.tsx
@@ -0,0 +1,55 @@
+'use client'
+import React, { useState } from 'react'
+import Heading from '../utils/Heading'
+import Header from '../components/Header'
+import Footer from '../components/footer/Footer'
+
+type Props = {}
+
+const Page = (props: Props) => {
+
+    const [route, setRoute] = useState('Login')
+    const [open, setOpen] = useState(false)
+    return (
+        <div className='min-h-screen'>
+            <Heading
+                title='Policy'
+                description='Elearning is programming community'
+                keyword='Programming community coding skills expert insights collaborating growth' />
+            <Header
+                route={route}
+                setRoute={setRoute}
+                open={open}
+                setOpen={setOpen}
+                activeItem={3} />
+
+            <div className="w-full mb-8">
+
+
+                <div className="w-[95%] 800px:w-[90%] p-1 m-auto">
+                    <h1 className='font-Poppins  text-[30px] mt-10 text-black dark:text-white'>Platform Terms and Condition</h1>
+                    <p className='font-Poppins text-start text-[20px] mt-10 text-black dark:text-white'>Programming is the art of creating programs that can be run on a computer. Programming involves writing code, which is a set of instructions that tell a computer what to do. Programming can be done in many different languages, including C++, Java, Python, and JavaScript.</p>
+
+
+                    <p className='font-Poppins text-start text-[20px] mt-10 text-black dark:text-white'>Programming involves writing code, which is a set of instructions that tell a computer what to do. Programming can be done in many different languages, including C++, Java, Python, and JavaScript.</p>
+
+                    <p className='font-Poppins text-start text-[20px] mt-10 text-black dark:text-white'>Our YouTube channel is a treasure trove of informative videos on everything from programming basics to advanced techniques. But that's just the beginning. Our affordable courses are designed to give you the high-quality education you need to succeed in the industry, without breaking the bank.</p>
+
+                    <p className='font-Poppins text-start text-[20px] mt-10 text-black dark:text-white'> we're a family. Our supportive community of like-minded individuals is here to help you every step of the way, whether you're just starting out or looking to take your skills to the next level.</p>
+                    <p className='font-Poppins text-start text-[20px] mt-10 text-black dark:text-white'>Our courses and community will provide you with the guidance, support, and motivation you need to unleash your full potential and become a skilled programmer.</p>
+                    <p className='font-Poppins text-start text-[20px] mt-10 text-black dark:text-white'>Are you ready to take your programming skills to the next level? Look no further than  the premier programming community dedicated to helping new programmers achieve their goals and reach their full potential.</p>
+
+
+
+                </div>
+
+
+
+
+            </div>
+            <Footer />
+        </div>
+    )
+}
+
+export default Page
